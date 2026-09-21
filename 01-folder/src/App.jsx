906ffb4850298e66a9f0a014dd78a1c4c9cf3388
Krishna <kp985203@gmail.com>
2026-09-21 17:00:51 +0530

    01 folder created

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+        <h2>Krishna</h2>
+      </div>
+
+      <div>
+        <h2>new to react</h2>
+      </div>
+    </>
+  )
+}
+export default App
